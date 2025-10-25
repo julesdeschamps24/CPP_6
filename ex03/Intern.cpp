f364cb6346a412ff58d81f876975b94f2ac7a786
judescha <julesdeschamps24@gmail.com>
2025-10-25 09:30:38 +0200

    fix ex03

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -1,18 +1,28 @@
 #include "Intern.hpp"
-#include <PresidentialPardonForm.hpp>
-#include <RobotomyRequestForm.hpp>
-#include <ShrubberyCreationForm.hpp>
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include <string>
+
+typedef AForm* (*Creator)(const std::string);
+
+static AForm* createShrubbery(const std::string target) { return (new ShrubberyCreationForm(target)); }
+static AForm* createRobotomy(const std::string target) { return (new RobotomyRequestForm(target)); }
+static AForm* createPresidential(const std::string target) { return (new PresidentialPardonForm(target)); }
 
 AForm* Intern::makeForm(std::string name, std::string target) {
-	if(name == "shrubbery creation")
-		return(new ShrubberyCreationForm(target));
-	if(name == "robotomy request")
-		return (new RobotomyRequestForm(target));
-	if(name == "presidential pardon")
-		return (new PresidentialPardonForm(target));
+	static const std::string str_tab[] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+	static Creator c[3] = {&createShrubbery, &createRobotomy, &createPresidential};
+
+	for (int i = 0; i != 3; i++)
+	{
+		if(name == str_tab[i])
+			return (c[i](target));
+	}
+	
 	throw UnknowName();
 }
 
 const char* Intern::UnknowName::what() const throw() {
-	return ("Unkmown name passed as parameter");
+	return ("Error: Unkmown name passed as parameter");
 }
